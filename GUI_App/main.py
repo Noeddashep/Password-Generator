360f69ad451cd3b207a90f0296a1517d8625e41b
Noeddasheep <muceteo@gmail.com>
2023-11-27 14:47:27 +0000

    add Readme

--- GUI_App/main.py
+++ GUI_App/main.py
@@ -1,9 +1,11 @@
+import tkinter
 import tkinter as tk
 import random
 import string
 from tkinter import *
 from PIL import Image, ImageTk
 
+# Create a new window
 window = tk.Tk()
 window.title("Password generator")
 window.columnconfigure([0, 1, 2, 3, 4], minsize=50, weight=1)
@@ -11,7 +13,10 @@ window.rowconfigure([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], minsize=50, weight=1)
 
 
 # logo
-def logo():
+def logo() -> None:
+    """
+    Opens an image and places it on the window
+    """
     logo_img = Image.open("GUI_App/images/images.png")
     logo_img = ImageTk.PhotoImage(logo_img)
     label = tk.Label(image=logo_img)
@@ -20,15 +25,29 @@ def logo():
 
 
 # Create a scale that defines the length of the password
-def length_password():
+def length_password() -> tkinter.IntVar:
+    """
+    Set the "Choose the length of your password" label and a scroll bar in the window. The bar is arranged horizontally
+    and includes a scale from 0 to 100.
+
+    return: the number got from the scale
+    """
     tk.Label(window, text="Choose the length of your password", font=("Arial", 13)).grid(row=0, column=1)
     scale_value = IntVar()
     scale = Scale(window, from_=0, to=100, orient=HORIZONTAL, font=("Arial", 13), sliderlength=20, variable=scale_value)
     scale.grid(row=0, column=2)
     return scale_value
 
+
 # Characters to select
-def character_checkboxes():
+def character_checkboxes() -> tuple[IntVar, IntVar, IntVar, IntVar]:
+    """
+    Set the label "What characters would you like to use for your password?\n" "Select one of the following options:"
+    on the window.
+    Create 4 checkboxes (numbers, lowercase, uppercase, and symbols) and set them on the window.
+
+    return: a tuple containing the values of the variables number, capital_letters, lowercase, symbol
+    """
     tk.Label(window, text="What characters would you like to use for your password?\n"
                           "Select one of the following options:", font=("Arial", 13)).grid(row=2, column=1)
 
@@ -48,15 +67,21 @@ def character_checkboxes():
 
 
 # label
-def password_label():
+def password_label() -> Label:
+    """
+    return: the label "Press "Generate password" to create the password\n' '\nSelect "Show password" to display it!"
+    """
     label_password = tk.Label(master=window, text='Press "Generate password" to create the password\n'
                                                   '\nSelect "Show password" to display it!', font=("Arial", 15))
     label_password.grid(row=8, column=1)
     return label_password
 
 
-# A function that generate a password
-def generate_password():
+def generate_password() -> None:
+    """
+    A function that generate a password. It gets the values of the variables numbers, capital_letters, lowercase, symbol
+    and show_password, by replacing the contents of the "label_password" label with the created password.
+    """
     length = int(scale_value.get())
     characters = ""
 
@@ -95,16 +120,20 @@ def generate_password():
         label_password["text"] = f"{password}"
 
 
-# A button allows to generate a password
-def generate_button():
+def generate_button() -> Button:
+    """
+    return: A button connected to the "generate_password" function, that allows to generate a password
+    """
     button = Button(window, text="Generate password", font=("Arial", 13), command=generate_password,
                     background="light grey")
     button.grid(row=6, column=1, sticky="nsew")
     return button
 
 
-# A box allows to display the password
-def show_password():
+def show_password() -> IntVar:
+    """
+    return: A box allows to display the password
+    """
     show_password = IntVar()
     Checkbutton(window, text="Show password", variable=show_password, font=("Arial", 13)).grid(row=6, column=0)
     return show_password
